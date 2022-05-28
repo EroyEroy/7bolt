739401e1dc59738bfa9e3f9a6f47cc8c0846855d
EroyEroy <vladislavtersenev7@gmail.com>
2022-05-28 13:37:28 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -16,46 +16,80 @@ function init() {
   rollSlider();
 
   const isMobile = window.matchMedia("only screen and (max-width: 479px)");
-  const slider = document.querySelector('.slider');
+
   if (isMobile.matches) {
-    slider.addEventListener("touchstart", handleTouchStart, false);
-    slider.addEventListener("touchmove", handleTouchMove, false);
+    // sliderLine.addEventListener("touchstart", handleTouchStart, false);
+    // sliderLine.addEventListener("touchmove", handleTouchMove, false);
 
-    let x1 = null;
-    let y1 = null;
+    // let x1 = null;
+    // let y1 = null;
 
-    function handleTouchStart(event) {
-      const firstTouch = event.touches[0];
-      // вытягивание координат клика
-      x1 = firstTouch.clientX;
-      y1 = firstTouch.clientY;
+    // function handleTouchStart(event) {
+    //   const firstTouch = event.touches[0];
+    //   // вытягивание координат клика
+    //   x1 = firstTouch.clientX;
+    //   y1 = firstTouch.clientY;
+    // }
+    // function handleTouchMove(event) {
+    //   if (!x1 || !y1) {
+    //     return false;
+    //   }
+    //   // координаты того, куда пользователь тянет
+    //   let x2 = event.touches[0].clientX;
+    //   let y2 = event.touches[0].clientY;
+
+    //   // нахождение разницы координат (в какую сторону)
+    //   let xDiff = x2 - x1;
+    //   let yDiff = y2 - y1;
+    //   // движиние вправо и влево
+    //   if (Math.abs(xDiff) >= Math.abs(yDiff)) {
+    //     console.log(xDiff, yDiff);
+    //     if (xDiff > 0) {
+    //       scrollSlide(-1);
+    //     } else if (xDiff < 0){
+    //       scrollSlide(1);
+    //     }
+    //   }
+    //   x1 = null;
+    //   y1 = null;
+    // }
+	let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+sliderLine.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+sliderLine.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    if (touchendX <= touchstartX) {
+        scrollSlide(1)
     }
-    function handleTouchMove(event) {
-      if (!x1 || !y1) {
-        return false;
-      }
-      // координаты того, куда пользователь тянет
-      let x2 = event.touches[0].clientX;
-      let y2 = event.touches[0].clientY;
-
-      // нахождение разницы координат (в какую сторону)
-      let xDiff = x2 - x1;
-      let yDiff = y2 - y1;
-      // движиние вправо и влево
-      if (Math.abs(xDiff) >= Math.abs(yDiff)) {
-        console.log(xDiff, yDiff);
-        if (xDiff > 0) {
-          scrollSlide(-1);
-        } else {
-          scrollSlide(1);
-        }
-      } else {
-		handleTouchStart(true);
-		handleTouchMove(true);
-	  }
-      x1 = null;
-      y1 = null;
+    
+    if (touchendX >= touchstartX) {
+        scrollSlide(-1);
     }
+    
+    if (touchendY <= touchstartY) {
+        console.log('Swiped up');
+    }
+    
+    if (touchendY >= touchstartY) {
+        console.log('Swiped down');
+    }
+    
+    if (touchendY === touchstartY) {
+        console.log('Tap');
+    }
+}
   }
 }
 var timer = 0;
