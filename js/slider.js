64d23d330bcfe73e861e97433e74f2c862339b73
EroyEroy <vladislavtersenev7@gmail.com>
2022-05-28 11:58:12 +0300

    slider fix

--- js/slider.js
+++ js/slider.js
@@ -65,45 +65,43 @@ function rollSlider() {
 function getWindowWidth() {
   return window.innerWidth || document.body.clientWidth;
 }
-window.addEventListener('resize', function(event){
-	if (getWindowWidth() <= 479) {
-		sliderLine.addEventListener("touchstart", handleTouchStart, false);
-		sliderLine.addEventListener("touchmove", handleTouchMove, false);
-		
-		let x1 = null;
-		let y1 = null;
-		
-		function handleTouchStart(event) {
-		  const firstTouch = event.touches[0];
-		  // вытягивание координат клика
-		  x1 = firstTouch.clientX;
-		  y1 = firstTouch.clientY;
-		}
-		function handleTouchMove(event) {
-		  if (!x1 || !y1) {
-			return false;
-		  }
-		  // координаты того, куда пользователь тянет
-		  let x2 = event.touches[0].clientX;
-		  let y2 = event.touches[0].clientY;
-		
-		  // нахождение разницы координат (в какую сторону)
-		  let xDiff = x2 - x1;
-		  let yDiff = y2 - y1;
-		  // движиние вправо и влево
-		  if (Math.abs(xDiff) >= Math.abs(yDiff)) {
-			console.log(xDiff, yDiff);
-			if (xDiff > 0) {
-			  scrollSlide(-1);
-			} else {
-			  scrollSlide(1);
-			}
-		  }
-		  x1 = null;
-		  y1 = null;
-		}
-	}
-  });
+if (getWindowWidth() <= 479) {
+  sliderLine.addEventListener("touchstart", handleTouchStart, false);
+  sliderLine.addEventListener("touchmove", handleTouchMove, false);
+
+  let x1 = null;
+  let y1 = null;
+
+  function handleTouchStart(event) {
+    const firstTouch = event.touches[0];
+    // вытягивание координат клика
+    x1 = firstTouch.clientX;
+    y1 = firstTouch.clientY;
+  }
+  function handleTouchMove(event) {
+    if (!x1 || !y1) {
+      return false;
+    }
+    // координаты того, куда пользователь тянет
+    let x2 = event.touches[0].clientX;
+    let y2 = event.touches[0].clientY;
+
+    // нахождение разницы координат (в какую сторону)
+    let xDiff = x2 - x1;
+    let yDiff = y2 - y1;
+    // движиние вправо и влево
+    if (Math.abs(xDiff) >= Math.abs(yDiff)) {
+      console.log(xDiff, yDiff);
+      if (xDiff > 0) {
+        scrollSlide(-1);
+      } else {
+        scrollSlide(1);
+      }
+    }
+    x1 = null;
+    y1 = null;
+  }
+}
 
 // свайп на пк версии
 // let startMousePos = 0;
