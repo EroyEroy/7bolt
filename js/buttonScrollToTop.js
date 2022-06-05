2159c111eaca4798429e305ca5ec98e9a4b36caa
EroyEroy <vladislavtersenev7@gmail.com>
2022-06-05 16:15:44 +0300

    Update buttonScrollToTop.js

--- js/buttonScrollToTop.js
+++ js/buttonScrollToTop.js
@@ -1,15 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let btn = document.querySelector(".scroll");
-  window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 1000) {
-      btn.classList.add("scroll_active");
-    } else {
-      btn.classList.remove("scroll_active");
-    }
-  });
-
-  btn.onclick = function (click) {
-    click.preventDefault();
-    scrollTo(0, 0);
-  };
-});
+const isBtnScroll = window.matchMedia("only screen and (min-width: 480px)");
+if (isBtnScroll.matches) {
+	document.addEventListener("DOMContentLoaded", function () {
+		let btn = document.querySelector(".scroll");
+		window.addEventListener("scroll", function () {
+		  if (window.pageYOffset > 1000) {
+			btn.classList.add("scroll_active");
+		  } else {
+			btn.classList.remove("scroll_active");
+		  }
+		});
+	  
+		btn.onclick = function (click) {
+		  click.preventDefault();
+		  scrollTo(0, 0);
+		};
+	  });	
+} else {
+	document.addEventListener("DOMContentLoaded", function () {
+		let btn = document.querySelector(".scroll");
+		window.addEventListener("scroll", function () {
+		  if (window.pageYOffset > 300) {
+			btn.classList.add("scroll_active");
+		  } else {
+			btn.classList.remove("scroll_active");
+		  }
+		});
+	  
+		btn.onclick = function (click) {
+		  click.preventDefault();
+		  scrollTo(0, 0);
+		};
+	  });
+}
