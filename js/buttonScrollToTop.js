746a2cc36b9c33c7c6594fa21aa512af759239e6
EroyEroy <vladislavtersenev7@gmail.com>
2022-06-05 15:57:24 +0300

    fix button scroll

--- js/buttonScrollToTop.js
+++ js/buttonScrollToTop.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let btn = document.querySelector('.scroll');
-    window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 1000) {
-            btn.classList.add('scroll_active');
-        } else {
-            btn.classList.remove('scroll_active');
-        }
-    });
-
-    btn.onclick = function (click) {
-        click.preventDefault();
-        scrollTo(0, 0);
+document.addEventListener("DOMContentLoaded", function () {
+  let btn = document.querySelector(".scroll");
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 1000) {
+      btn.classList.add("scroll_active");
+    } else {
+      btn.classList.remove("scroll_active");
     }
+  });
+
+  btn.onclick = function (click) {
+    click.preventDefault();
+    scrollTo(0, 0);
+  };
 });
