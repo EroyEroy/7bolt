9b52ed33597b8a27abda4aebb1b517bea53ad4e0
EroyEroy <vladislavtersenev7@gmail.com>
2022-06-29 16:14:05 +0300

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -145,7 +145,7 @@ window.addEventListener("touchend", () => {
   if (startX + 100 < moveX) {
     // console.log('right');
     console.log(1);
-  } else if (startX > moveX-400) {
+  } else if (startX > moveX-150) {
     // console.log('left');
     console.log(-1);
 	catalogBackground.forEach(item => {
