d29168631ee754f49f936fefe08afcf87a9d14da
EroyEroy <vladislavtersenev7@gmail.com>
2022-06-29 17:06:59 +0300

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -141,7 +141,7 @@ window.addEventListener("touchmove", (e) => {
   }
 });
 window.addEventListener("touchend", () => {
-  if (startX < moveX-300) {
+  if (startX < moveX-200) {
 	console.log(startX, moveX);
     // console.log('right');
 	mobileCatalogBtn.click();
